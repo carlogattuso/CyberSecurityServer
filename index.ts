0ef00e94551032865c0894354ca9839afb8cbef6
carlogattuso <carlogc46@gmail.com>
2020-03-28 21:27:52 +0100

    message 1 sent

--- index.ts
+++ index.ts
@@ -4,7 +4,7 @@ import bodyParser from 'body-parser';
 import router from './routes/index';
 import cors from 'cors';
 
-const port: number = 3000;
+const port: number = 50000;
 const app: express.Application = express();
 
 app.use(cors({origin:'http://localhost:4200'}));
